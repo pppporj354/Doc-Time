dcc26ec6bf4ec592225521cd60a2b67752381fb3
Shifin-Malik <shifinmalik46@gmail.com>
2024-12-10 03:55:55 -0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DocTime/', // Ensure this matches your repo name
+  base: '/Doc-Time/', // Ensure this matches your repo name
   plugins: [react()],
 })
